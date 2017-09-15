45323b7ee120ed9d3be82736bc1216fa830511b1
Relyn13 <rbdc13@gmail.com>
2017-09-16 06:30:29 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Auth::routes();
 
 
 Route::get('/home', 'HomeController@index')->name('home');
-=
+
 Route::get('/create_transaction', 'PostController@index');
 Route::get('/process', 'PostController@create');
 
