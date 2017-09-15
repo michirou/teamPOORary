757d125f30c6c164d35df89df37fbee80575d464
jeffrxn <obero.jeffersonbutch.ssc10@gmail.com>
2017-09-15 22:07:17 +0800

    login new temp UI

--- resources/views/auth/register_backup.php
+++ resources/views/auth/register_backup.php
@@ -5,20 +5,17 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2 flex-center position-ref full-height">
             <div class="panel panel-default" id="flat-panel">
-                <!-- <div class="panel-heading"> -->
+                <div class="panel-heading">
                     <!-- <span>Login</span> -->
-                    <!-- <span class="glyphicon glyphicon-user"></span> -->
-                <!-- </div> -->
+                    <span class="glyphicon glyphicon-user"></span>
+                </div>
 
-                <div class="panel-body" style="text-align: center;">
-                    <div style="color: #10635F; font-size: 300%; margin: 10%;">
-                        <span class="glyphicon glyphicon-piggy-bank"></span>
-                    </div>
+                <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
-                           <!--  <label for="email" class="col-md-4 control-label">E-Mail</label>
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
@@ -28,23 +25,11 @@
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                            </div> -->
-                            <div class="col-xs-10 col-xs-offset-1">
-                                <div class="input-group">
-                                    <span class="input-group-addon" id="user-addon"><span class="glyphicon glyphicon-user"></span></span>
-                                    <input type="text" class="form-control" name="username" id="username" placeholder="Username" aria-describedby="#user-addon" />
-                                </div>
                             </div>
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <div class="col-xs-10 col-xs-offset-1">
-                                <div class="input-group">
-                                    <span class="input-group-addon" id="pass-addon"><span class="glyphicon glyphicon-lock"></span></span>
-                                    <input type="password" class="form-control" name="username" id="username" placeholder="Password" aria-describedby="#pass-addon" />
-                                </div>
-                            </div>
-                            <!-- <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
@@ -54,10 +39,10 @@
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                            </div> -->
+                            </div>
                         </div>
 
-<!--                         <div class="form-group">
+                        <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
@@ -65,17 +50,17 @@
                                     </label>
                                 </div>
                             </div>
-                        </div> -->
+                        </div>
 
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-default" id="flat-btn" style="width: 50%; margin-top: 10%;">
+                                <button type="submit" class="btn btn-default" id="flat-btn">
                                     <span>Login</span>
                                 </button>
 
-                                <!-- <a id="btn-link" class="btn btn-link" href="{{ route('password.request') }}">
+                                <a id="btn-link" class="btn btn-link" href="{{ route('password.request') }}">
                                     Forgot Your Password?
-                                </a> -->
+                                </a>
                             </div>
                         </div>
                     </form>
