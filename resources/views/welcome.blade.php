1a62ff2bcffb4744bf1f24eb58961a5ee8b8ab43
jeffrxn <obero.jeffersonbutch.ssc10@gmail.com>
2017-09-15 15:39:30 +0800

    bootstrap integration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>temp</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -13,10 +13,14 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                /*background-color: #fff;*/
+                /*background-color: #FF7055;*/
+                background-color: #FCB410;
+                /*color: #636b6f;*/
+                color: #FFFFF7;
                 font-family: 'Raleway', sans-serif;
-                font-weight: 100;
+                /*font-weight: 100;*/
+                font-weight: bold;
                 height: 100vh;
                 margin: 0;
             }
@@ -50,7 +54,8 @@
             }
 
             .links > a {
-                color: #636b6f;
+                /*color: #636b6f;*/
+                color: #402E04;
                 padding: 0 25px;
                 font-size: 12px;
                 font-weight: 600;
@@ -59,11 +64,23 @@
                 text-transform: uppercase;
             }
 
+            .desc > span {
+                /*color: #636b6f;*/
+                color: #402E04;
+                /*padding: 0 25px;*/
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
             .m-b-md {
-                margin-bottom: 30px;
+                /*margin-bottom: 30px;*/
             }
         </style>
     </head>
+    <!-- <body style="background-color: #FF7055; color: #FFFFF7;"> -->
     <body>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
@@ -79,16 +96,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <span>Leihen</span>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="desc">
+                    <span>Borrow anytime. anywhere. At your convenience.</span>
                 </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">asd</div>
             </div>
         </div>
     </body>
