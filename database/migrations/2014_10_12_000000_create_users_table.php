3a3ab5033b2b9a24513e7811cdbd01cb682edfc6
Relyn13 <rbdc13@gmail.com>
2017-09-15 15:51:28 +0800

    r

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('username');
             $table->string('password');
+            $table->string('first-name');
+            $table->string('middle-name');
+            $table->string('last-name');
+            $table->string('address');
+            $table->string('card-number');
+            $table->string('social-media-acc');
+            $table->string('work');
+            $table->string('type-of-work');
+            $table->string('salary');
+            $table->string('numyears-working');
+            $table->string('credit-score');
+            $table->string('social-score');
             $table->rememberToken();
             $table->timestamps();
         });
