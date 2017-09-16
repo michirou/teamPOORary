ace8288ebf149ed5d52bb50f80e792f2af345b2b
jeffrxn <obero.jeffersonbutch.ssc10@gmail.com>
2017-09-16 09:59:19 +0800

    newsfeed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,56 +1,86 @@
 @extends('layouts.app')
 
 @section('content')
-<<<<<<< HEAD
 <div class="container-fluid" style="margin-top: -1%;">
     <div class="row">
         <div style="width: 100%;">
             <div class="panel panel-default" id="flat-panel">
                 <div class="panel-heading" style="overflow: hidden;">
                     <div class="pull-right">
-                        <button class="btn btn-default" id="flat-btn"><span>New Post</span></button>
+                        <button class="btn btn-default" id="flat-btn" onclick="window.location.href='/lend_money'"><span>Lend Money</span></button>
+                        <button class="btn btn-default" id="flat-btn" onclick="window.location.href='/borrow_money'"><span>Borrow Money</span></button>
                     </div>
-                    <!-- Dashboard -->
-                    <!-- <a href="/create_transaction">Create Transaction</a> -->
-                    <!-- <a href="/view_transaction">View Transactions from Peers</a> -->
-<div class="container">
-
-    <!-- static palang please -->
-
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <a href="/lend_money" class="btn btn-primary">Lend Money</a>
-             <a href="/borrow_money" class="btn btn-primary">Borrow Money</a>
-
-
-            <div class="panel panel-default">
-                <div class="panel-heading">Person One (to Lend)</div>
-                <div class="panel-body">
-                      <!--   <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div> -->
-                    
-                    Amount: PHP 1000.00<br />
-                    Interest: 10%<br />
-                    Meeting Place: Iloilo Convention Center
                 </div>
-            </div>
 
-            <div class="panel panel-default">
-                <div class="panel-heading">Person Two (to Borrow)</div>
-                <div class="panel-body">
-                    Amount: PHP 500.00<br />
-                    Interest: 5%<br />
-                    Meeting Place: SM Iloilo
+                <div class="panel panel-default" id="feed-panel">
+                        <div class="panel-heading">Person One (to Lend)</div>
+                        <div class="panel-body">                            
+                            Amount: PHP 1000.00<br />
+                            Interest: 10%<br />
+                            Meeting Place: Iloilo Convention Center
+                        </div>
+                    </div>
+                <div class="panel panel-default" id="feed-panel" style="margin-top: -5%;">
+                        <div class="panel-heading">Person One (to Lend)</div>
+                        <div class="panel-body">                            
+                            Amount: PHP 1000.00<br />
+                            Interest: 10%<br />
+                            Meeting Place: Iloilo Convention Center
+                        </div>
+                    </div>
+
+                <div class="panel panel-default" id="feed-panel" style="margin-top: -5%;">
+                    <div class="panel-heading">Person Two (to Borrow)</div>
+                    <div class="panel-body">
+                        Amount: PHP 500.00<br />
+                        Interest: 5%<br />
+                        Meeting Place: SM Iloilo
+                    </div>
                 </div>
-            </div>
 
-            <div class="panel panel-default">
-                <div class="panel-heading">Person 3 (to Lend)</div>
-                <div class="panel-body">
-                    Amount: PHP 10000.00<br />
-                    Interest: 15%<br />
-                    Meeting Place: Somewhere else
+                <div class="panel panel-default" id="feed-panel" style="margin-top: -5%;">
+                    <div class="panel-heading">Person 3 (to Lend)</div>
+                    <div class="panel-body">
+                        Amount: PHP 10000.00<br />
+                        Interest: 15%<br />
+                        Meeting Place: Somewhere else
+                    </div>
+                </div>
+
+                <div class="panel panel-default" id="feed-panel" style="margin-top: -5%;">
+                    <div class="panel-heading">Person 3 (to Lend)</div>
+                    <div class="panel-body">
+                        Amount: PHP 10000.00<br />
+                        Interest: 15%<br />
+                        Meeting Place: Somewhere else
+                    </div>
+                </div>
+
+                <div class="panel panel-default" id="feed-panel" style="margin-top: -5%;">
+                    <div class="panel-heading">Person 3 (to Lend)</div>
+                    <div class="panel-body">
+                        Amount: PHP 10000.00<br />
+                        Interest: 15%<br />
+                        Meeting Place: Somewhere else
+                    </div>
+                </div>
+
+                <div class="panel panel-default" id="feed-panel" style="margin-top: -5%;">
+                    <div class="panel-heading">Person 3 (to Lend)</div>
+                    <div class="panel-body">
+                        Amount: PHP 10000.00<br />
+                        Interest: 15%<br />
+                        Meeting Place: Somewhere else
+                    </div>
+                </div>
+
+                <div class="panel panel-default" id="feed-panel" style="margin-top: -5%;">
+                    <div class="panel-heading">Person 3 (to Lend)</div>
+                    <div class="panel-body">
+                        Amount: PHP 10000.00<br />
+                        Interest: 15%<br />
+                        Meeting Place: Somewhere else
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,16 +62,16 @@
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
                             <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
+                                <a href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                                            <span>Logout</span>
+                                </a>
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
                             <!-- <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
